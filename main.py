2900b4185c085780b22d64792f4337cb8a078daf
PRANAV_REDDY <reddysaipranav6@gmail.com>
2025-04-14 12:31:40 +0530

    latest-updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import uvicorn
 loaded_model = joblib.load('best_model_gb.pkl')
 
-app = FastAPI(title='Loan Defaulter Prediction')
+app = FastAPI(title='Loan Defaulter Prediction1')
 
 class Prediction(BaseModel):
     person_age: int
