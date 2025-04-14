0b276709c836d72287f6a3a22075c0b0e6a52f78
PRANAV_REDDY <reddysaipranav6@gmail.com>
2025-04-14 11:39:31 +0530

    Updated-Version

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,11 @@
+import sys
+import os
+
+# Add the root directory to sys.path so that the import from main works
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi.testclient import TestClient
-from main import app 
+from main import app
 
 client = TestClient(app)
 
